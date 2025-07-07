3bf2ebb14367ba7bee107506013bb718d5dd11f5
Yeder <ypimentel.tapia@gmail.com>
2025-07-07 17:36:57 +0200

    feat(model)created the trip model to save routes

--- server/models/trip.py
+++ server/models/trip.py
@@ -1,1 +1,15 @@
-#empezamos a crear el backend
+from extensions import db
+from datetime import datetime
+
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    stopped_time = db.Column(db.Float, nullable=False)
+    moving_time = db.Column(db.Float, nullable=False)
+    suitcase_count = db.Column(db.Integer, default=0)
+    total = db.Column(db.Float, nullable=False)
+
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship("User", back_populates="trips")
